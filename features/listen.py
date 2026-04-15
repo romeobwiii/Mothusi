10fd54544982cb9c75cc4f8651ed701a8f39a547
Mulax Prime <161124347+romeobwiii@users.noreply.github.com>
2026-04-15 01:41:03 -0700

    Update listen.py

--- features/listen.py
+++ features/listen.py
@@ -17,4 +17,23 @@ def listen():
         return ""
     except sr.RequestError as e:
         print(f"No internet connection: {e}")
+        return ""#listen.py Handels all the voice input for Mothusi 
+import speech_recognition as sr
+from features.speak import speak
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source, duration=0.3)  # Adjust for ambient noise
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+        print("You said: " + command)
+        return command.lower()
+    except sr.UnknownValueError:
+        print("Sorry.")
+        return ""
+    except sr.RequestError as e:
+        print(f"No internet conne ction: {e}")
         return ""
